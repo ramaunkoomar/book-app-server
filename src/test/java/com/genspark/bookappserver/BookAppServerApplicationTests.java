c54a0100561a5d37b06822a5ad71cbf84e0990cf
thomasmunguya <thomasmunguya@gmail.com>
2022-09-24 16:41:49 +0200

    Initial commit.

---
+++ src/test/java/com/genspark/bookappserver/BookAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.bookappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
